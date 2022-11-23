cc150c65efefacb7a6cfc1f18501c54516ab6f85
rouphen <roy@Roy.local>
2022-11-23 16:04:54 +0800

    refactory test-case

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -3,21 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	assertCorrectMessaeg := func(t *testing.T, got, want string) {
+		t.Helper()
 
 		if got != want {
 			t.Errorf("got '%q' want '%q'", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessaeg(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessaeg(t, got, want)
 	})
 }
