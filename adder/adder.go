86423c2885deac93bea0c075ed1d16f6b6b21b69
rouphen <roy@Roy.local>
2022-11-24 17:08:20 +0800

    updated the package name

--- adder/adder.go
+++ adder/adder.go
@@ -1,6 +1,11 @@
-package integers
+package main
 
+import "fmt"
 // Add takes two integers and returns the sum of them
 func Add(x, y int) int {
 	return x + y
+}
+
+func main() {
+	fmt.Println(Add(3, 1))
 }
