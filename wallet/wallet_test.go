1c2eca979e432f586ba98c38212803b3b17c1dcf
rouphen <roy@Roy.local>
2022-11-25 18:30:06 +0800

    added func Withdraw

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -3,15 +3,31 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	bt := Bitcoin(10)
-	wallet.Deposit(bt)
-
-	got := wallet.Balance()
-	want := bt
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	checkWallet := func(t *testing.T, got, want Bitcoin) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
 
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		bt := Bitcoin(10)
+		wallet.Deposit(bt)
+
+		got := wallet.Balance()
+		want := bt
+
+		checkWallet(t, got, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(10)
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		checkWallet(t, got, want)
+	})
 }
