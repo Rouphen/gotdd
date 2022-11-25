445921bc4538944c5071e8e9cf2e8a5df9c4b978
rouphen <roy@Roy.local>
2022-11-25 18:45:28 +0800

    refacoried message

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -3,8 +3,11 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	checkWallet := func(t *testing.T, got, want Bitcoin) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		t.Helper()
+
+		got := wallet.Balance()
+
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
@@ -12,22 +15,27 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
-		bt := Bitcoin(10)
-		wallet.Deposit(bt)
+		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := bt
-
-		checkWallet(t, got, want)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(10)
+		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-		checkWallet(t, got, want)
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
 	})
 }
