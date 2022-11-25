1b82ecf29fddb043dc73d42f13fb00b0673a4bd8
rouphen <roy@Roy.local>
2022-11-25 15:59:29 +0800

    added func Area

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,5 +1,9 @@
 package shapes
 
-func Perimeter(x, y float64) float64 {
-	return (x + y) * 2
+func Perimeter(width, height float64) float64 {
+	return (width + height) * 2
+}
+
+func Area(width, height float64) float64 {
+	return width * height
 }
