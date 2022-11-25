8c9efbae921641f6c33519d0571112acac8bbd42
rouphen <roy@Roy.local>
2022-11-25 20:25:11 +0800

    changed to custom type Dictionary

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -3,9 +3,9 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	got := Dictionary.Search(dictionary, "test")
 	want := "this is just a test"
 
 	if got != want {
