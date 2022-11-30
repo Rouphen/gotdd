49f4d0518ed88218ad654a577f674cfe36a33a1d
rouphen <roy@Roy.local>
2022-11-30 15:37:54 +0800

    refactoried the func Walk

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,11 +3,7 @@ package reflection
 import "reflect"
 
 func Walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
@@ -19,4 +15,14 @@ func Walk(x interface{}, fn func(input string)) {
 			Walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
