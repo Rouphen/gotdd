f64504e8081c70e4b01022582c9054456c2c0f14
rouphen <roy@Roy.local>
2022-12-01 11:57:54 +0800

    added server for showing player score

---
+++ server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	fmt.Fprint(w, getPlayerScore(player))
+}
+
+func getPlayerScore(player string) string {
+	if player == "Pepper" {
+		return "20"
+	}
+
+	if player == "Floyd" {
+		return "10"
+	}
+
+	return "no player"
+}
