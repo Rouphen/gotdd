f3f03bd76aafbc8fb44e35ad517ced9100239e1e
rouphen <roy@Roy.local>
2022-12-02 11:15:57 +0800

    added router

---
+++ json/myprogram.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+func main() {
+	server := &PlayerServer{NewInMemoryPlayerStore()}
+	handler := http.HandlerFunc(server.ServeHTTP)
+
+	if err := http.ListenAndServe(":5008", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
